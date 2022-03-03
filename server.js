1fb4f78351f92d051cbd4636432d3d634f28377b
nyhasina <nyhasina.finaritra@gmail.com>
2022-03-03 16:47:18 +0300

    final commit 2.0

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/bucketListItems/' , bucketListItemsRoutes)
 if(process.env.NODE_ENV === 'production'){
         app.use(express.static('client/dist'))
         app.get('*' , (req , res) => {
-                res.sendFile(path.resolve(__dirname , 'client', 'dist' , 'index.html'))
+                res.sendFile(path.join(`${__dirname}/client/build`));
         })
 }
 
